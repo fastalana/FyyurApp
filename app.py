51258883488681803a8a2b14170639332adfd752
Alana Bellucci <fastalana@gmail.com>
2020-06-05 12:12:12 -0600

    Testing templates for front end.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def home_view():
-    return "<h1>Hello, World!</h1>"
+    # return "<h1>Hello, World!</h1>"
+    return render_template('pages/home.html')
