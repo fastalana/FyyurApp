9e799a6e3b34c2d41397b84707863113df30ac2e
Alana Bellucci <fastalana@gmail.com>
2020-06-05 13:31:44 -0600

    Adding models and testing migrations locally.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
+from models import setup_db, db
 from flask_cors import CORS
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    setup_db(app)
     CORS(app)
 
     @app.route("/")
